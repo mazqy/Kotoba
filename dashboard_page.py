68a3eba8e38d74f1de6c3d6cc31fdf2e929a3af9
mazqy <mazqyyy@gmail.com>
2025-02-09 16:22:39 +0100

    Better Dashboard and new window icon

--- dashboard_page.py
+++ dashboard_page.py
@@ -1,5 +1,15 @@
-from PyQt6.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QSpacerItem, QSizePolicy
+from PyQt6.QtWidgets import (
+    QWidget,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+    QHBoxLayout,
+    QSpacerItem,
+    QSizePolicy,
+)
 from PyQt6 import QtCore, QtGui
+
+decks = ["Deck 1", "Deck 2", "Deck 3", "Deck 4", "Deck 5"]
 
 
 class DashboardPage(QWidget):
@@ -9,64 +19,83 @@ class DashboardPage(QWidget):
 
         self.main_Vbox_layout = QVBoxLayout()
         self.decks_Vbox_layout = QVBoxLayout()
-        
+
         self.top_container = QWidget()
         self.top_container_layout = QHBoxLayout()
 
         self.top_container.setLayout(self.top_container_layout)
 
-    
         self.label_title = QLabel("Dashboard")
 
-        self.label_title.setStyleSheet("""
+        self.label_title.setStyleSheet(
+            """
     font-size: 28px;
     color: white;
     font-weight: bold;
 
-""")
+"""
+        )
         self.label_title.setAlignment(QtCore.Qt.AlignmentFlag.AlignHCenter)
-        self.label_title.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.label_title.setSizePolicy(
+            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding
+        )
 
         self.label_streak = QLabel("Streak: 0")
 
-        self.label_streak.setStyleSheet("""
+        self.label_streak.setStyleSheet(
+            """
     font-size: 20px;
     color: white;
     font-weight: bold;
 
-""")
+"""
+        )
         self.label_streak.setAlignment(QtCore.Qt.AlignmentFlag.AlignHCenter)
-        self.label_streak.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.label_streak.setSizePolicy(
+            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding
+        )
 
-        
-        
         self.button_start_review = QPushButton("Start review")
-        self.button_start_review.setStyleSheet("""
+        self.button_start_review.setStyleSheet(
+            """
     background-color: green;
     color: white;
     border-radius: 10px;
     padding: 10px;
     font-size: 16px;
                                                
-""")
+"""
+        )
 
         self.button_start_review.clicked.connect(self.go_to_review_page)
-        
-        self.top_container.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+
+        self.top_container.setSizePolicy(
+            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed
+        )
+        self.top_container.setStyleSheet("background-color: blue")
         self.top_container_layout.addStretch(0)
         self.top_container_layout.addWidget(self.label_title)
         self.top_container_layout.addWidget(self.label_streak)
 
+        for deck in decks:
+            self.deck_info_container = QWidget()
+            self.deck_info_container.setStyleSheet("background-color: green")
+            self.deck_info_container_layout = QHBoxLayout()
+            self.deck_info_container.setLayout(self.deck_info_container_layout)
 
-        self.decks_Vbox_layout.addWidget(QLabel("Deck 1"))
-        self.decks_Vbox_layout.addWidget(QLabel("deck 2"))
-        self.decks_Vbox_layout.addWidget(QLabel("Deck 3"))
+            self.deck_info_container.setSizePolicy(
+                QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed
+            )
+
+            self.deck_info_container_layout.addWidget(QLabel(deck))
+            self.deck_info_container_layout.addWidget(QLabel("70%"))
+            self.decks_Vbox_layout.addWidget(self.deck_info_container)
 
         self.main_Vbox_layout.addWidget(self.top_container)
         self.main_Vbox_layout.addLayout(self.decks_Vbox_layout)
+        self.main_Vbox_layout.addStretch()
         self.main_Vbox_layout.addWidget(self.button_start_review)
-        
-        
+
         self.setLayout(self.main_Vbox_layout)
 
     def go_to_review_page(self):
